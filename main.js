2b34019a1a232012538a2b9a11daea105ed8a61f
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-02-20 20:18:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-(() => {
+//(() => {
     let lives = 5, alreadyGivenCharacters = [], secretWord = [];
 
     const ALLOWED_CHARACTERS = 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρστυφχψωabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('');
@@ -187,4 +187,4 @@
     addToPage(wordInput);
     addToPage(startButton);
     document.getElementById('boxesHolder').appendChild(hintText);
-})();
+//})();
