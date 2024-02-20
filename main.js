cc37041d24308877a1bf687e396a60286cc45c51
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-02-20 15:11:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 (() => {
+
+    const addToPage = (element) => {
+        document.getElementById('periexomeno').appendChild(element);
+    }
+
     // Συνάρτηση αφαίρεσης άχρηστων κενών
     const removeRedundantSpaces = str => {
         const searchTerm2 = "   ", // Triple spaces.
@@ -15,37 +20,38 @@
         return tmpStr;
     };
 
-    const space = '_';
+    const setup = () => {
+        // Δες αν ο χρήστης έδωσε λέξη
+        chosenWord = removeRedundantSpaces(document.getElementById('thingyUenterword').value);
+        switch (true) {
+            case chosenWord == '' || chosenWord == ' ':
+                alert('Ρε φίλε πρέπει να δώσεις και μία κανονική λέξη..');
+                break;
+        }
 
+        // Δημιούργησε το μενού με τα κενά
+        let wordThing = document.createElement('h2');
+        wordThing.style.letterSpacing = '4px';
+        wordThing.style.pointerEvents = 'none';
+
+        for (var i = 0; i < chosenWord.length; i++) {
+            wordThing.innerText += '_';
+        }
+
+        addToPage(wordThing);
+    }
+
+    // Πλαίσιο για να εισάγεται η λέξη στην αρχή.
     let chosenWord;
 
     let wordInput = document.createElement('input');
     wordInput.type = 'text';
+    wordInput.id = 'thingyUenterword';
     wordInput.style.width = '100%';
     wordInput.placeholder = 'Εισάγετε μία λέξη εδώ..';
     wordInput.addEventListener('keypress', (e) => {
-        if (e.keyCode == 27) {
-            chosenWord = wordInput.value;
-        }
-    })
+        if (e.keyCode == 27) setup();
+    });
 
-    // Δες αν ο χρήστης έδωσε λέξη
-    switch (true) {
-        case chosenWord == '' || chosenWord == ' ':
-            alert('Ρε φίλε πρέπει να δώσεις και μία κανονική λέξη..');
-            break;
-    }
-
-    // Δημιούργησε το μενού με τα κενά
-    let wordThing = document.createElement('h2');
-    wordThing.style.letterSpacing = '4px';
-    wordThing.style.pointerEvents = 'none';
-
-    for (var i = 0; i < chosenWord.length; i++) {
-        wordThing.innerText += space;
-    }
-
-
-
-    document.getElementById('periexomeno').appendChild(wordThing);
+    addToPage(wordInput);
 })();
