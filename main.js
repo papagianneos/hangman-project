6b3beeb5a149900a9f51569be553187a9463c7a7
papagianneos <71824952+papagianneos@users.noreply.github.com>
2024-02-20 13:58:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 (() => {
   const space = '_';
+  let contentsOfWord = '';
   
   let chosenWord = prompt('Πληκτρολογίστε μία λέξη για την κρεμάλα: ');
 
   let wordThing = document.createElement('h2');
 
   for (var i = 0; i < chosenWord.length; i++) {
-    wordThing.innerText += space;
-    alert('done');
+    contentsOfWord += space;
   }
 
+  wordThing.appendChild(document.createTextNode(contentsOfWord));
   documnent.getElementById('periexomeno').appendChild(wordThing);
 })();
